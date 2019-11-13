ab4aaf222bec0012bc18bd4eda0371468a25f11f
Iurii <kenu21@gmail.com>
2019-11-13 21:26:53 +0200

    initial

---
+++ src/main/java/com/smida/stockregister/StockRegisterApplication.java
@@ -0,0 +1,12 @@
+package com.smida.stockregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockRegisterApplication.class, args);
+    }
+}
